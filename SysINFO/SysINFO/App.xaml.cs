87f721c9bd044f05afff3695a76466eb63209b49
ZarryStudy <80962678+ZarryStudy@users.noreply.github.com>
2022-12-28 18:44:16 +0700

    Update App.xaml.cs

--- SysINFO/SysINFO/App.xaml.cs
+++ SysINFO/SysINFO/App.xaml.cs
@@ -10,6 +10,7 @@ namespace SysINFO
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
+    /// Изменения в ветке1-1
     /// </summary>
     public partial class App : Application
     {
